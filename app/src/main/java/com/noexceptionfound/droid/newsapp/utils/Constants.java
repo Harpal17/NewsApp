df53778a419dddc804fc69f61b098ff5719d74df
Harpal <harpal17.singh@gmail.com>
2019-01-25 11:44:13 +0530

    Initial commit : prepared first & complete stage of app flow.

---
+++ app/src/main/java/com/noexceptionfound/droid/newsapp/utils/Constants.java
@@ -0,0 +1,13 @@
+package com.noexceptionfound.droid.newsapp.utils;
+
+/**
+ * Created by Harpal on 12/7/2018.
+ */
+
+public class Constants {
+    public static final String BASE_URL  = "https://newsapi.org/v2/";
+    public static final String LANGUAGE  = "sources?language=en";
+    public static final String API_KEY_STR = "&apiKey=";
+
+    public static final String API_KEY  = "32db12b875074e1c919a3149d78c21f9";
+}
